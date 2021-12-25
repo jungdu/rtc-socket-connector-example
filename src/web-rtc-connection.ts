cd7e414d2f57f01947a6bbcf87b3bf7595974873
jungdujang <jungdujang@gmail.com>
2021-12-25 18:50:27 +0900

    Chore: add comments

--- src/web-rtc-connection.ts
+++ src/web-rtc-connection.ts
@@ -2,14 +2,17 @@ import { createRTCConnectionManager, RTCConnectionHandler } from "rtc-socket-con
 import {io} from "socket.io-client"
 
 function main(){
+  // Get HTML elements
   const connectedSocketId = document.getElementById("connectedSocketId") as HTMLSpanElement;
   const connectBtn = document.getElementById("connectBtn") as HTMLButtonElement;
   const targetInput = document.getElementById("targetInput") as HTMLInputElement;
   const socketId = document.getElementById("socketId") as HTMLSpanElement;
 
+  // 1. Connect to socket.io server
   const socket = io("http://localhost:5000");
   socket.on("connect", handleSocketConnect);
 
+  // 2. Define RTCConnectionHandler
   const rtcConnectionHandler: RTCConnectionHandler = {
     onRTCPeerConnection: (socketId, rtcPeerConnection) => {
       rtcPeerConnection.addEventListener("connectionstatechange", () => {
@@ -21,9 +24,11 @@ function main(){
     }
   }
 
+  // 3. Create RTCConnectionManager
   const rtcConnectionManager = createRTCConnectionManager(socket, rtcConnectionHandler);
 
   connectBtn.addEventListener('click', () => {
+    // 4. Connect to another client
     rtcConnectionManager.connect(targetInput.value, {
       enableDataChannel: true
     });
